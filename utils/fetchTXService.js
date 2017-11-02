45621a5046c92986b5e52546fb542a754f7e518e
zyevegor <zyev.egor@gmail.com>
2017-11-02 12:48:54 +0300

    https://chronobank.atlassian.net/browse/MINT-659 - add litecoin support

--- utils/fetchTXService.js
+++ utils/fetchTXService.js
@@ -16,7 +16,7 @@ module.exports = async hash => {
 
   Object.assign(ipcInstance.config, {
     id: Date.now(),
-    socketRoot: config.bitcoin.ipcPath,
+    socketRoot: config.node.ipcPath,
     retry: 1500,
     sync: true,
     silent: true,
@@ -25,22 +25,22 @@ module.exports = async hash => {
   });
 
   await new Promise((res, rej) => {
-    ipcInstance.connectTo(config.bitcoin.ipcName, () => {
-      ipcInstance.of[config.bitcoin.ipcName].on('connect', res);
-      ipcInstance.of[config.bitcoin.ipcName].on('error', rej);
+    ipcInstance.connectTo(config.node.ipcName, () => {
+      ipcInstance.of[config.node.ipcName].on('connect', res);
+      ipcInstance.of[config.node.ipcName].on('error', rej);
     });
   });
 
   let tx = await new Promise((res, rej) => {
-    ipcInstance.of[config.bitcoin.ipcName].on('message', data => data.error ? rej(data.error) : res(data.result));
-    ipcInstance.of[config.bitcoin.ipcName].emit('message', JSON.stringify({
+    ipcInstance.of[config.node.ipcName].on('message', data => data.error ? rej(data.error) : res(data.result));
+    ipcInstance.of[config.node.ipcName].emit('message', JSON.stringify({
       method: 'getrawtransaction',
       params: [hash, true]
     })
     );
   });
 
-  ipcInstance.disconnect(config.bitcoin.ipcName);
+  ipcInstance.disconnect(config.node.ipcName);
 
   return tx;
 
