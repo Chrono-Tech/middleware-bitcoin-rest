d89d594fb1cf1062aff2154003c404c424a25242
zyevegor <zyev.egor@gmail.com>
2017-12-28 12:23:42 +0300

    https://chronobank.atlassian.net/browse/MD-72 - bitcoin bugs and features

--- tests/index.js
+++ tests/index.js
@@ -11,10 +11,8 @@ const config = require('../config'),
   expect = require('chai').expect,
   accountModel = require('../models/accountModel'),
   ipcExec = require('./helpers/ipcExec'),
-  _ = require('lodash'),
   Promise = require('bluebird'),
   request = Promise.promisify(require('request')),
-  Coin = require('bcoin/lib/primitives/coin'),
   scope = {},
   mongoose = require('mongoose');
 
@@ -29,10 +27,8 @@ describe('core/rest', function () {
 
     let keyPair = bcoin.hd.generate(ctx.network);
     let keyPair2 = bcoin.hd.generate(ctx.network);
-    let keyPair3 = bcoin.hd.generate(ctx.network);
-    let keyPair4 = bcoin.hd.generate(ctx.network);
 
-    ctx.accounts.push(keyPair, keyPair2, keyPair3, keyPair4);
+    ctx.accounts.push(keyPair, keyPair2);
 
     mongoose.connect(config.mongo.accounts.uri, {useMongoClient: true});
   });
@@ -45,15 +41,11 @@ describe('core/rest', function () {
 
     let keyring = new bcoin.keyring(ctx.accounts[0].privateKey, ctx.network);
     let keyring2 = new bcoin.keyring(ctx.accounts[1].privateKey, ctx.network);
-    let keyring3 = new bcoin.keyring(ctx.accounts[2].privateKey, ctx.network);
-    let keyring4 = new bcoin.keyring(ctx.accounts[3].privateKey, ctx.network);
 
     return await accountModel.remove({
       address: {
         $in: [keyring.getAddress().toString(),
-          keyring2.getAddress().toString(),
-          keyring3.getAddress().toString(),
-          keyring4.getAddress().toString()]
+          keyring2.getAddress().toString()]
       }
     })
   });
@@ -64,8 +56,8 @@ describe('core/rest', function () {
     return await ipcExec('generatetoaddress', [50, keyring.getAddress().toString()])
   });
 
-  it('unlock coins for account A by generating some coins for accountD', async () => {
-    let keyring = new bcoin.keyring(ctx.accounts[3].privateKey, ctx.network);
+  it('unlock coins for account A by generating some coins for accountB', async () => {
+    let keyring = new bcoin.keyring(ctx.accounts[1].privateKey, ctx.network);
     let amqpInstance = await amqp.connect(config.rabbit.url);
     let channel = await amqpInstance.createChannel();
     try {
@@ -86,8 +78,9 @@ describe('core/rest', function () {
     })
   });
 
-  it('register addresses', async () => {
 
+  it('register addresses', async () => {
+    await Promise.delay(10000);
     let responses = await Promise.all(ctx.accounts.map(account => {
       let keyring = new bcoin.keyring(account.privateKey, ctx.network);
       return request({
@@ -105,83 +98,25 @@ describe('core/rest', function () {
     )
   });
 
-  it('send coins to accountB and accountC', async () => {
-    await Promise.delay(60000);
-
-    let keyring = new bcoin.keyring(ctx.accounts[0].privateKey, ctx.network);
-    let keyring2 = new bcoin.keyring(ctx.accounts[1].privateKey, ctx.network);
-    let keyring3 = new bcoin.keyring(ctx.accounts[2].privateKey, ctx.network);
-    let coins = await ipcExec('getcoinsbyaddress', [keyring.getAddress().toString()]);
-    let height = await ipcExec('getblockcount', []);
-
-    let inputCoins = _.chain(coins)
-      .transform((result, coin) => {
-        if (height - coin.height < 6)
-          return;
-
-        result.coins.push(Coin.fromJSON(coin));
-        result.amount += coin.value;
-      }, {amount: 0, coins: []})
-      .value();
-
-    const mtx = new bcoin.mtx();
-
-    mtx.addOutput({
-      address: keyring2.getAddress(),
-      value: Math.pow(10, 8)
-    });
-
-    mtx.addOutput({
-      address: keyring3.getAddress(),
-      value: Math.pow(10, 8) * 3
-    });
-
-    await mtx.fund(inputCoins.coins, {
-      rate: 10000,
-      changeAddress: keyring.getAddress()
-    });
-
-    mtx.sign(keyring);
-    const tx = mtx.toTX();
-    scope.mtx = mtx;
-
-    let response = await request({
-      url: `http://${config.rest.domain}:${config.rest.port}/tx/send`,
-      method: 'post',
-      json: {
-        tx: tx.toRaw().toString('hex')
-      }
-    });
-
-    expect(response.body).to.include.any.keys('hash', 'fee');
-
-  });
-
-  it('generate 1 block', async () => {
-    let keyring = new bcoin.keyring(ctx.accounts[3].privateKey, ctx.network);
-    return await ipcExec('generatetoaddress', [1, keyring.getAddress().toString()])
-  });
-
   it('validate potential balance changes for accounts', async () => {
-    await Promise.delay(60000);
     let keyring = new bcoin.keyring(ctx.accounts[0].privateKey, ctx.network);
     let keyring2 = new bcoin.keyring(ctx.accounts[1].privateKey, ctx.network);
-    let keyring3 = new bcoin.keyring(ctx.accounts[2].privateKey, ctx.network);
 
-    let accountA = await accountModel.findOne({address: keyring.getAddress().toString()});
-    let accountB = await accountModel.findOne({address: keyring2.getAddress().toString()});
-    let accountC = await accountModel.findOne({address: keyring3.getAddress().toString()});
+    let responses = await Promise.all([
+      keyring.getAddress().toString(),
+      keyring2.getAddress().toString()
+    ].map(address => {
+      return request({
+        url: `http://${config.rest.domain}:${config.rest.port}/addr/${address}/balance`,
+        method: 'get',
+        json: true
+      })
+    }));
 
-    let diffA = _.get(accountA, 'balances.confirmations6', 0) - _.get(accountA, 'balances.confirmations0', 0);
-    let diffB = _.get(accountB, 'balances.confirmations0', 0) - _.get(accountB, 'balances.confirmations6', 0);
-    let diffC = _.get(accountC, 'balances.confirmations0', 0) - _.get(accountC, 'balances.confirmations6', 0);
-
-    scope.balanceA0 = _.get(accountA, 'balances.confirmations0', 0);
-    scope.balanceB0 = _.get(accountB, 'balances.confirmations0', 0);
-    scope.balanceC0 = _.get(accountC, 'balances.confirmations0', 0);
-
-    expect(diffB + diffC + scope.mtx.getFee()).to.equal(diffA);
-
+    responses.forEach(resp =>
+      expect(resp.body.confirmations0.satoshis).to.be.above(1)
+    )
   });
+
 
 });
