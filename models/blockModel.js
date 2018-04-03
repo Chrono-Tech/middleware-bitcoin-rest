6637fd64bbeb732c578e038a3620c2fa40de0418
zyevegor <zyev.egor@gmail.com>
2018-04-03 13:41:10 +0300

    https://chronobank.atlassian.net/browse/MD-260 - bitcoin-middleware: move txs to separate collection

--- models/blockModel.js
+++ models/blockModel.js
@@ -11,24 +11,6 @@ const Block = new mongoose.Schema({
   number: {type: Number, unique: true, index: true},
   hash: {type: String, unique: true, index: true},
   timestamp: {type: Number, required: true, index: true},
-  txs: [{
-    value: {type: String},
-    hash: {type: String, index: true},
-    fee: {type: String},
-    minFee: {type: String},
-    inputs: [{
-      prevout:  {
-        hash: {type: String, index: true},
-        index: {type: Number}
-      },
-      value: {type: String},
-      address: {type: String, index: true}
-    }],
-    outputs: [{
-      value: {type: String},
-      address: {type: String, index: true}
-    }]
-  }],
   network: {type: String},
   created: {type: Date, required: true, default: Date.now}
 });
